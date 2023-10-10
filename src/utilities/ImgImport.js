15704677aead9aea8ac88a76679d6e9954cd0df3
Đỗ Lê Anh Khoa <khoadole21@gmail.com>
2023-10-10 15:26:24 +0700

    yeah image blog

---
+++ src/utilities/ImgImport.js
@@ -0,0 +1,5 @@
+export const Home = "/src/assets/images/HomePage/HomePageCard.png";
+export const Contact = "/src/assets/images/AboutMePage/AboutMePageCard.png";
+export const SlapKyarun =
+  "/src/assets/images/SlapKyarunPage/SlapKyarunCard.png";
+export const Blog = "/src/assets/images/BlogPage/BlogPageCard.png";
